a2af35fa32587f613fc8a6581368492562a9b23b
EDafe Ukoh <ukohme@gmail.com>
2023-11-24 22:31:43 +0000

    added names

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Welcome to London");
-    System.out.println("Hello, Welcome to London");
-    System.out.println("Hello, Welcome to London");
-    System.out.println("Hello, Welcome to London");
-    System.out.println("Hello, Welcome to London");
+    System.out.println("Hello Steve, Welcome to London");
+    System.out.println("Hello Sam, Welcome to London");
+    System.out.println("Hello Ken, Welcome to London");
+    System.out.println("Hello John, Welcome to London");
+    System.out.println("Hello Paul, Welcome to London");
   }
 }
