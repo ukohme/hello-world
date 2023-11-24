3c3a4f303e040e93fca879c9b78b4b66de124fa5
EDafe Ukoh <ukohme@gmail.com>
2023-11-24 22:06:22 +0000

    poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, Welcome to London");
     System.out.println("Hello, Welcome to London");
     System.out.println("Hello, Welcome to London");
   }
